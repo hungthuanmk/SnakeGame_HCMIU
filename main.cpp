a396945d76520b673128e712d840533f9189e4db
ntxtung <xuantung.1998@gmail.com>
2017-07-10 10:26:14 +0700

    Fix Console
    - Fix console buffer

--- main.cpp
+++ main.cpp
@@ -1,59 +1,3 @@
-/*
----------LOG-----------
-- First C/C++ class (26/06/2017), project game making: Caro, Sudoku, Snake
---> Snake challenge accepted!
--------BUILD LOG-------
-******26/06/2017*******
-- Research snake game making on internet
-- Build a wall around
-******27/06/2017*******
-- Build a snake head can move around and snake properties
-- Food spawning (a big bug)
-******28/06/2017*******
-- Big bug removed (still some)
-- Snake tail build (luckily! just some bug)
-******29/06/2017*******
-- IU Snake ver 0.01beta completed with ONE bug (go left on the start will kill snake :) )
-    Feature:
-    - A simple snake game
-    - Snake can move around map, eat food, die when touch wall or eat himself :)
-    - Snake move vertical fasterrrrr...
-    - Scoring system included
-    - No annoying cursor mess up
-    - No long delay
-    - Just 1.0 MB :)
-
-******30/06/2017*******
-- IU Snake ver 0.02beta updated
-    - Snake can go through safe border (no bug detected)
-    - Score board added
-
-******06/07/2017*******
-- Fix some lag when foodSpawn
-
-*/
-
-/*
-Name         | Value
-             |
-Black        |   0
-Blue         |   1
-Green        |   2
-Cyan         |   3
-Red          |   4
-Magenta      |   5
-Brown        |   6
-Light Gray   |   7
-Dark Gray    |   8
-Light Blue   |   9
-Light Green  |   10
-Light Cyan   |   11
-Light Red    |   12
-Light Magenta|   13
-Yellow       |   14
-White        |   15
-*/
-
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>  // to use system function
@@ -63,65 +7,70 @@ White        |   15
 #include <vector>    // to build Snake
 #include <tchar.h>
 
-#define W 40   //Screen Width
-#define H 20   //Screen Height
-#define scrX 5    //Screen LeftTop X
-#define scrY 2    //Screen LeftTop Y
+#define version "0.3beta"
+#define Debug   false
+#define ConsoleTitles "IU Snake - By Xuan Tung and HTML *** Updated: 06/07/2017"
 
-#define KEY_UP 72
-#define KEY_DOWN 80
-#define KEY_LEFT 75
-#define KEY_RIGHT 77
+#define W       40   //Screen Width
+#define H       20   //Screen Height
+#define scrX    5    //Screen LeftTop X
+#define scrY    2    //Screen LeftTop Y
 
-#define foodIcon 254
-#define snakeHeadIcon 178
-#define snakeTailIcon 176
+#define KEY_UP      72
+#define KEY_DOWN    80
+#define KEY_LEFT    75
+#define KEY_RIGHT   77
 
-#define defaultColor 15
-#define snakeHeadColor 2
-#define snakeTailColor 14
-#define foodIcon 15
+#define foodIcon        254
+#define snakeHeadIcon   178
+#define snakeTailIcon   176
 
-
-#define ConsoleTitles "IU Snake 0.02b - By Xuan Tung and HTML *** Updated: 30/06/2017"
+#define defaultColor     15
+#define snakeHeadColor   2
+#define snakeTailColor   14
 
 #define MAX_TAIL (W-2)*(H-2)
 
+/*
+ +Name         | Value
+ +             |
+ +Black        |   0
+ +Blue         |   1
+ +Green        |   2
+ +Cyan         |   3
+ +Red          |   4
+ +Magenta      |   5
+ +Brown        |   6
+ +Light Gray   |   7
+ +Dark Gray    |   8
+ +Light Blue   |   9
+ +Light Green  |   10
+ +Light Cyan   |   11
+ +Light Red    |   12
+ +Light Magenta|   13
+ +Yellow       |   14
+ +White        |   15
+ +*/
+
 using namespace std;
 
-char screen[H+1][W+1]={' '};
-int zone[H+1][W+1]={0};
+char screen[H+1][W+1] = {' '};
+int zone[H+1][W+1] = {0};
 
-int score=0, snakeX=(W/2), snakeY=(H/2), foodX=0, foodY=0, gameSpeed=30, timer=0;
-int numTails=1;
+int score = 0, snakeX = (W/2), snakeY = (H/2), foodX = 0, foodY = 0, gameSpeed = 40, timer = 0;
+int numTails = 1;
 
 vector <int> tailX, tailY;
 
-char direct='S';
-bool gameOver=false;
-bool gameStart=false;
+char direct ='S';
+bool gameOver = false;
+bool gameStart = false;
 /*---------------Header--------------------*/
 void foodSpawn();
-void infoBoard ();
+void infoBoard();
 
 /*---------------RootFunc------------------*/
 // This Function set cursor to (x,y)
-void SetTextColor(int ForgC)
- {
-     WORD wColor;
-
-      HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-      CONSOLE_SCREEN_BUFFER_INFO csbi;
-
-                           //We use csbi for the wAttributes word.
-     if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
-     {
-                     //Mask out all but the background attribute, and add in the forgournd color
-          wColor = (csbi.wAttributes & 0xF0) + (ForgC & 0x0F);
-          SetConsoleTextAttribute(hStdOut, wColor);
-     }
- }
-
 void gotoxy(int x, int y)
 {
     static HANDLE hi = NULL;
@@ -142,10 +91,25 @@ void ShowConsoleCursor(bool showFlag)
     SetConsoleCursorInfo(wHnd, &cursorInfo);
 }
 
+void SetTextColor(int ForgC)
+{
+    WORD wColor;
+
+    HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+                        //We use csbi for the wAttributes word.
+    if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
+    {
+                    //Mask out all but the background attribute, and add in the forgournd color
+        wColor = (csbi.wAttributes & 0xF0) + (ForgC & 0x0F);
+        SetConsoleTextAttribute(hStdOut, wColor);
+    }
+}
+
 void ConsoleSetup ()
 {
-    HANDLE wHnd=GetStdHandle(STD_OUTPUT_HANDLE);    // Write on console
-    HANDLE rHnd=GetStdHandle(STD_INPUT_HANDLE);    // Read from console
+    HANDLE wHnd = GetStdHandle(STD_OUTPUT_HANDLE);    // Write on console
+    HANDLE rHnd = GetStdHandle(STD_INPUT_HANDLE);    // Read from console
     CONSOLE_CURSOR_INFO cursorInfo;
 
     // Change init color
@@ -160,121 +124,125 @@ void ConsoleSetup ()
 
     SetConsoleTitle(TEXT(ConsoleTitles));
 
+    SetConsoleOutputCP(437);
+
     // Resize setup
 
-    SMALL_RECT windowSize = {0, 0, 80, 50};
+    SMALL_RECT windowSize = {0, 0, 100, 50};
 
     // Resize screen
 
     SetConsoleWindowInfo(wHnd, TRUE, &windowSize);
 
     // Resize buffer
-    COORD bufferSize = {80, 50};
+    COORD bufferSize = {73, 25};
     SetConsoleScreenBufferSize(wHnd, bufferSize);
 
     // Set default code page
     SetConsoleOutputCP(487);
 }
 
+
+
 /*---------------InitFunc------------------*/
+// Start Screen border
+void startScreenBorder()
+{
+
+}
 // Make Danger border and zone
 void makeDangerBorder()
 {
     // Top
     for (int x=0; x<W; x++)
     {
-        screen[0][x]=char(205);
-        zone[0][x]=1;
+        screen[0][x] = char(205);
+        zone[0][x] = 1;
     }
     // Left
     for (int y=0; y<H; y++)
     {
-        screen[y][0]=char(186);
-        zone[y][0]=1;
+        screen[y][0] = char(186);
+        zone[y][0] = 1;
     }
     // Bottom
     for (int x=0; x<W; x++)
     {
-        screen[H][x]=char(205);
-        zone[H][x]=1;
+        screen[H][x] = char(205);
+        zone[H][x] = 1;
     }
     // Right
     for (int y=0; y<H; y++)
     {
-        screen[y][W]=char(186);
-        zone[y][W]=1;
+        screen[y][W] = char(186);
+        zone[y][W] = 1;
     }
     // TopLeft
-    screen[0][0]=char(201); zone[0][0]=1;
+    screen[0][0] = char(201); zone[0][0]=1;
     // TopRight
-    screen[0][W]=char(187); zone[0][W]=1;
+    screen[0][W] = char(187); zone[0][W]=1;
     // BottomLeft
-    screen[H][0]=char(200); zone[H][0]=1;
+    screen[H][0] = char(200); zone[H][0]=1;
     // BottomRight
-    screen[H][W]=char(188); zone[H][W]=1;
+    screen[H][W] = char(188); zone[H][W]=1;
 }
 
 // Safezone Border
 void makeSafeBorder()
 {
     // Top
-    for (int x=0; x<W; x++) screen[0][x]=char(196);
+    for (int x=0; x<W; x++) screen[0][x] = char(196);
     // Left
-    for (int y=0; y<H; y++) screen[y][0]=char(179);
+    for (int y=0; y<H; y++) screen[y][0] = char(179);
     // Bottom
-    for (int x=0; x<W; x++) screen[H][x]=char(196);
+    for (int x=0; x<W; x++) screen[H][x] = char(196);
     // Right
-    for (int y=0; y<H; y++) screen[y][W]=char(179);
+    for (int y=0; y<H; y++) screen[y][W] = char(179);
     // TopLeft
-    screen[0][0]=char(218);
+    screen[0][0] = char(218);
     // TopRight
-    screen[0][W]=char(191);
+    screen[0][W] = char(191);
     // BottomLeft
-    screen[H][0]=char(192);
+    screen[H][0] = char(192);
     // BottomRight
-    screen[H][W]=char(217);
+    screen[H][W] = char(217);
 }
+
 void safeBorderLogic()
 {
     // Left side
-    if (snakeX==W && zone[snakeY][snakeX]!=1)
+    if (snakeX == W && zone[snakeY][snakeX] != 1)
     {
-        screen[snakeY][snakeX]=char(179);
-        snakeX=1;
+        screen[snakeY][snakeX] = char(179);
+        snakeX = 1;
     }
     // Right side
-    if (snakeX==0 && zone[snakeY][snakeX]!=1)
+    if (snakeX == 0 && zone[snakeY][snakeX]!=1)
     {
         screen[snakeY][snakeX]=char(179);
-        snakeX=W-1;
+        snakeX = W-1;
     }
     // Bot side
-    if (snakeY==H && zone[snakeY][snakeX]!=1)
+    if (snakeY == H && zone[snakeY][snakeX]!=1)
     {
-        screen[snakeY][snakeX]=char(196);
-        snakeY=1;
+        screen[snakeY][snakeX] = char(196);
+        snakeY = 1;
     }
     // Top side
-    if (snakeY==0 && zone[snakeY][snakeX]!=1)
+    if (snakeY == 0 && zone[snakeY][snakeX]!=1)
     {
-        screen[snakeY][snakeX]=char(196);
-        snakeY=H-1;
+        screen[snakeY][snakeX] = char(196);
+        snakeY = H-1;
     }
-
 }
-
-
 
 // 0 = safe
 // 1 = dangerous
 
-
 // Setup process
 void init()
 {
-
-    ConsoleSetup();
-
+    //ConsoleSetup();
 
     //makeDangerBorder();
     makeSafeBorder();
@@ -322,7 +290,7 @@ void debug ()
     int Yline=16;
 
     gotoxy(70,Yline);
-    cout << "IU Snake 0.03b";
+    cout << "IU Snake ver " << version;
     Yline++;
 
     gotoxy(70,Yline);
@@ -346,88 +314,68 @@ void debug ()
     Yline++;
 }
 
-
 void drawScreen(){
     for (int i=0; i<=H; i++)
     {
         gotoxy(scrX,scrY+i);
         for (int j=0; j<=W; j++)
-        {
-            /*if (screen[i][j]==char(snakeHeadIcon))
-            {
-                SetTextColor(snakeHeadColor);
-                cout << screen[i][j];
-                SetTextColor(defaultColor);
-            }
-            else
-                cout << screen[i][j];*/
-
+         {
             switch (screen[i][j])
             {
-            case char(snakeHeadIcon):
+                case char(snakeHeadIcon):
                 {
                     SetTextColor(snakeHeadColor);
                     cout << screen[i][j];
                     SetTextColor(defaultColor);
                     break;
                 }
-            case char(snakeTailIcon):
+                case char(snakeTailIcon):
                 {
                     SetTextColor(snakeTailColor);
                     cout << screen[i][j];
                     SetTextColor(defaultColor);
                     break;
                 }
-            case char(foodIcon):
-            default:
+                case char(foodIcon):
+                {
+                    SetTextColor(12);
+                    cout << screen[i][j];
+                    SetTextColor(defaultColor);
+                    break;
+                }
+                default:
                 cout << screen[i][j];
             }
+
         }
     }
 }
-
 
 // getKey --> snakeMove
 void getKey()
 {
     char tempDir;
-
     if (_kbhit()) //Keyboard Hit
     {
-        switch (_getch())
-        {
-        case 'A':
-        case 'a':
-        case KEY_LEFT:
-            tempDir='L';
-            break;
-        case 'S':
-        case 's':
-        case KEY_DOWN:
-            tempDir='D';
-            break;
-        case 'D':
-        case 'd':
-        case KEY_RIGHT:
-            tempDir='R';
-            break;
-        case 'W':
-        case 'w':
-        case KEY_UP:
-            tempDir='U';
-            break;
-        }
-        gameStart=true;
+        int kbKey = _getch();
+        if (kbKey == 'A' || kbKey == 'a' || kbKey == KEY_LEFT)
+            tempDir = 'L';
+        if (kbKey == 'S' || kbKey == 's' || kbKey == KEY_DOWN)
+            tempDir = 'D';
+        if (kbKey == 'D' || kbKey == 'd' || kbKey == KEY_RIGHT)
+            tempDir = 'R';
+        if (kbKey == 'W' || kbKey == 'w' || kbKey == KEY_UP)
+            tempDir = 'U';
+        //printf("%d",'a');
+     //   if (direct == 'S' && tempDir != 'L')
+            gameStart = true;
     }
-
-
 
     if (
         (tempDir=='D' && direct!='U') ||
         (tempDir=='U' && direct!='D') ||
         (tempDir=='L' && direct!='R') ||
         (tempDir=='R' && direct!='L')
-
         )
             direct=tempDir;
 }
@@ -456,33 +404,30 @@ void snakeMove()
 void makeSnake()
 {
 
-    for (int i=0; i<=numTails-1; i++) // Tail
+    for (int i = 0; i <= numTails-1; i++) // Tail
     {
-        screen[tailY[i]][tailX[i]]=char(snakeTailIcon);
-        zone[tailY[i]][tailX[i]]=1;
+        screen[tailY[i]][tailX[i]] = char(snakeTailIcon);
+        zone[tailY[i]][tailX[i]] = 1;
     }
-    screen[snakeY][snakeX]=char(snakeHeadIcon); //Head
-
-
+    screen[snakeY][snakeX] = char(snakeHeadIcon); //Head
 }
 
 void deleteSnake()
 {
-    screen[snakeY][snakeX]=' ';
+    screen[snakeY][snakeX] = ' ';
 }
 // Food Spawn (Fail)
 void foodSpawn()
 {
-
     do
     {
-        srand(foodX+foodY);
-        foodX=rand()%W;
-        foodY=rand()%H;
+        srand(foodX + foodY);
+        foodX = rand()%W;
+        foodY = rand()%H;
     }
     while (zone[foodY][foodX]!=0 || (foodX==snakeX && foodY==snakeY) || (foodX==0) || (foodY==0));
 
-    screen[foodY][foodX]=char(foodIcon);
+    screen[foodY][foodX] = char(foodIcon);
 }
 // Tail
 
@@ -491,7 +436,6 @@ void saveTail ()
     tailX.insert(tailX.begin(),snakeX);
     tailY.insert(tailY.begin(),snakeY);
     //deleteTail
-
     while (tailX.size()>numTails || tailY.size()>numTails)
     {
         screen[tailY[tailY.size()-1]][tailX[tailX.size()-1]]=' ';
@@ -499,14 +443,11 @@ void saveTail ()
         tailX.pop_back();
         tailY.pop_back();
     }
-
-
 }
 
 // Game Logical
 void logic()
 {
-
     safeBorderLogic();
 
     if (snakeX==foodX && snakeY==foodY)
@@ -517,12 +458,12 @@ void logic()
         foodSpawn();
     }
 
-    if (zone[snakeY][snakeX]==1) gameOver=true;
+    if (zone[snakeY][snakeX]==1)
+        gameOver=true;
 }
 // This draw
 void draw()
 {
-
     getKey();
 
     if (gameStart)
@@ -537,18 +478,16 @@ void draw()
         drawScreen();
 
 
-
         deleteSnake();
 
         saveTail();
 
-        timer+=gameSpeed;
-
+        timer += gameSpeed;
 
         Sleep(gameSpeed);
         infoBoard();
-        //debug();
-
+        if (Debug)
+            debug();
     }
     else
     {
@@ -569,32 +508,34 @@ void draw()
 
 void startScreen ()
 {
+    ConsoleSetup();
 
 }
 
-void mapSelect (){
+void mapSelect ()
+{
+
 }
 
 void playScreen ()
 {
     init();
-
     while (gameOver!=true)
         {
             draw();
-            ShowConsoleCursor(false);
         }
-
     gotoxy(0,23);
 }
 
-void resultScreen (){
+void resultScreen ()
+{
 }
 
 /*---------------MainFunc--------------------*/
 int main()
 {
+    startScreen();
     playScreen();
-    getch();
+
     return 0;
 }
