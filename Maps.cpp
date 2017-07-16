76f1f8071416f15736d13aca28c14f56aff8aae8
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2017-07-16 17:33:28 +0700

    Add Maps.h and Maps.cpp
    Add Maps Screen features

---
+++ Maps.cpp
@@ -0,0 +1,10 @@
+#ifndef CONSOLEAPI_H_INCLUDED
+    #include "ConsoleAPI.h"
+#endif // CONSOLEAPI_H_INCLUDED
+
+#include "Maps.h"
+#include <fstream>
+
+using namespace std;
+
+
---
+++ Maps.h
@@ -0,0 +1,10 @@
+#ifndef MAPS_H_INCLUDED
+#define MAPS_H_INCLUDED
+
+
+
+
+
+
+
+#endif // MAPS_H_INCLUDED
