ba4235fc9ca088325ff9c2da3f6590bc99e2c2ec
ntxtung <xuantung.1998@gmail.com>
2017-07-20 22:00:20 +0700

    Update Music

--- main.cpp
+++ main.cpp
@@ -2,17 +2,27 @@
 #include <conio.h>
 #include "Screens.h"
 #include <windows.h>
+#include <stdio.h>
+#include <mmsystem.h>
+#pragma comment(lib,"libwinmm.a")
 
 using namespace std;
 
 char ConsoleTitle[] = "IU Snake - By Xuan Tung and HTML *** Updated: 16/07/2017";
 char version[] = "0.4";
 
+
+
 int main()
 {
+
+    PlaySound(TEXT("soundtrack\\StartScreen.wav"), NULL, SND_FILENAME | SND_LOOP | SND_ASYNC);
     mainMenu();
     getch();
     system("cls");
+    PlaySound(NULL, 0, 0);
+
+
     playScreen();
     getch();
     return 0;
