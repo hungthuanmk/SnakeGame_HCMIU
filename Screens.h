373dcb8de2802dff3847e68e6481f4cf0d8242c5
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2017-07-21 11:29:37 +0700

    Demo210717 (#19)
    * DEMO UPDATE 21/07/17
    
    * DEMO UPDATE 11:21
    
    * Delete ConsoleAPI.o
    
    * Delete Declaration.o
    
    * Delete GamePlay.o
    
    * Delete Screens.o
    
    * Delete main.o
    
    * Delete main.o

--- Screens.h
+++ Screens.h
@@ -11,16 +11,34 @@
                QUIT GAME :(
 */
 
+/**
+    Show main Menu.
+*/
 void mainMenu();
 
+/**
+    Show game play screen.
+*/
 void playScreen();
 
+/**
+    Show map selection.
+*/
 void mapScreen();
 
+/**
+    Show about screen.
+*/
 void aboutScreen();
 
+/**
+    Sound result and highscore screen.
+*/
 void resultScreen();
 
+/**
+    Select menu at Main menu
+*/
 void selectMenu();
 
 #endif // SCREENS_H_INCLUDED
